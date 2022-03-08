e0bb57875bd9fee9972c6c9243eea01168545807
tuanhung220901 <hoangtuanhung220901@gmail.com>
2022-03-08 10:15:27 +0700

    up

---
+++ load.js
@@ -0,0 +1,13 @@
+// loading
+function myFunction() {
+    var element = document.getElementById("loader");
+    element.style.display = "none"
+  }
+  function my() {
+    setTimeout(myFunction, 3000);
+  }
+  my();
+// $(window).on("load", function (event) {
+//   $("body").removeClass("preloading");
+//   $("#loader").delay(1000).fadeOut("fast");
+// });
